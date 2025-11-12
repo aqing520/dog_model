903a7972676bf3e52379b59454138b1414eda783
wzy <2660828538@qq.com>
2025-11-12 21:14:34 +0800

    尝试编写launch文件,在加载stl网格文件时出错

--- install/DOG/share/DOG/launch/display.launch.py
+++ install/DOG/share/DOG/launch/display.launch.py
@@ -42,12 +42,12 @@ def generate_launch_description():
         executable='joint_state_publisher',
     )
 
-    #添加控制滑钮  如果gui参数为true 则启动joint_state_publisher_gui节点
-    joint_state_publisher_gui_node = launch_ros.actions.Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        condition=launch.conditions.IfCondition(launch.substitutions.LaunchConfiguration('gui'))
-    )
+    # #添加控制滑钮  如果gui参数为true 则启动joint_state_publisher_gui节点
+    # joint_state_publisher_gui_node = launch_ros.actions.Node(
+    #     package='joint_state_publisher_gui',
+    #     executable='joint_state_publisher_gui',
+    #     condition=launch.conditions.IfCondition(launch.substitutions.LaunchConfiguration('gui'))
+    # )
 
 
     # RViz 节点    启动rviz2  等价于 ros2 run rviz2 rviz2 -d <path_to_config>
@@ -63,6 +63,5 @@ def generate_launch_description():
         action_declare_arg_mode_path,
         joint_state_publisher_node,
         robot_state_publisher_node,
-        joint_state_publisher_gui_node,
         rviz_node
     ])
